de09fe2194274990003d681cb11f0a6880bac585
AaronSanki <aaronagasthyasanki@gmail.com>
2025-05-23 00:38:15 +0530

    make session persistent

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ app.use(session({
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "none",
+        // sameSite: "none",
         secure: process.env.NODE_ENV === "production"    
     }
 }))
