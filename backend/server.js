e72d9e68b391962c0ef3a4fd3d95a3e98c341ca4
AaronSanki <aaronagasthyasanki@gmail.com>
2025-05-22 17:19:13 +0530

    Add port to server

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,17 @@ import passport from "passport"
 import localStrategy from "passport-local"
 import User from "./models/user.js"
 import session from "express-session"
-
+import dotenv from "dotenv"
 //App config
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const corsOptions = {
     origin: "http://localhost:5173",
     methods: "GET, POST, PUT, DELETE",
     credentials: true
 }
+if(process.env.NODE_ENV !== "production")
+    dotenv.config()
 
 //Middleware
 app.use(express.json())
