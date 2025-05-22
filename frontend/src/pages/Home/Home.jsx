2b6f940717f0c20f63f3070de9ac562e4fde598f
AaronSanki <aaronagasthyasanki@gmail.com>
2025-05-22 18:34:39 +0530

    fix bugs

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from "react";
 import './Home.css'
-import Header from "../../Components/Header/Header";
-import FilterFrameShapes from "../../Components/FilterFrames/FilterFrameShapes.jsx";
-import FilterFrameTypes from "../../Components/FilterFrames/FilterFrameTypes.jsx";
-import FrameList from '../../Components/FrameList/FrameList.jsx'
-import AppDownload from "../../Components/AppDownload/AppDownload";
+import Header from "../../components/Header/Header";
+import FilterFrameShapes from "../../components/FilterFrames/FilterFrameShapes.jsx";
+import FilterFrameTypes from "../../components/FilterFrames/FilterFrameTypes.jsx";
+import FrameList from '../../components/FrameList/FrameList.jsx'
+import AppDownload from "../../components/AppDownload/AppDownload";
 import { useApp } from "../../store/index";
 export default function Home() {
     const { showSignIn } = useApp();
