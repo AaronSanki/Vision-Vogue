1fa655ec8aaa0933e117647aacca24a8b2286791
AaronSanki <aaronagasthyasanki@gmail.com>
2025-05-24 16:12:51 +0530

    change logo url

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import profileImage from '../../assets/ProfileImage.jpeg'
 export default function Navbar() {
     return (
         <div className="navbar py-3 d-flex justify-content-between align-items-center ">
-            <img className="/Logo.png" alt="Logo" />
+            <img className="logo" src="/Logo.png" alt="Logo" />
             <p>Admin-Panel</p>
             <div className="img-container">
                 <img className="profile-image" src={profileImage} alt="Profile Image" />
