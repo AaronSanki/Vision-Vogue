5780ad0ae4a13294f5e1f1540d1915fd7193e1b1
AaronSanki <aaronagasthyasanki@gmail.com>
2025-05-26 16:18:03 +0530

    remove additional cors

--- backend/routes/frame.js
+++ backend/routes/frame.js
@@ -2,19 +2,8 @@ import express from 'express'
 import { addFrame, listFrames, removeFrame, editFrame, fetchFrame } from '../controllers/frame.js'
 import {upload, validateFrame} from '../middleware/index.js'
 import wrapAsync from '../utils/wrapAsync.js'
-import cors from "cors"
 
 const router = express.Router()
-const corsOptions = {
-    origin: [process.env.FRONTEND_URL, process.env.ADMIN_URL],
-    methods: "GET, POST, PUT, DELETE, OPTIONS",
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-    exposedHeaders: ['Authorization'],
-    optionsSuccessStatus: 204
-}
-
-router.use(cors(corsOptions))
 
 router
     .route("/add")
