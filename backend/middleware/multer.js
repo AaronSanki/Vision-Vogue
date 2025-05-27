b4afa5e7be84d0a285461167a584cfb441473e35
AaronSanki <aaronagasthyasanki@gmail.com>
2025-05-28 00:32:09 +0530

    Add cloudinary for multer

--- backend/middleware/multer.js
+++
@@ -1,9 +0,0 @@
-import multer from "multer"
-const storage = multer.diskStorage({
-    destination: "uploads",
-    filename: (req, file, cb) => {
-        return cb(null, `${Date.now()}${file.originalname}`)
-    }
-})
-
-export default multer({storage})
